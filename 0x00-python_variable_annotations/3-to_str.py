fed6331b96e096cfd2f7097495ba64ade46b3c5f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-07 00:44:39 +0300

    c

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Contains float"""
+
+
+def to_str(n: float) -> str:
+    """Returns float
+    Args:
+        n: A string
+    Returns:
+        float
+    """
+    return str(n)
