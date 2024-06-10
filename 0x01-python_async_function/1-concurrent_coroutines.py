8c8da5642b20e1ece110bc894a4634bb8dc0b3c5
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-10 15:09:57 +0300

    c

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-""" Imposdrt wasd"""
+""" Imp ff df. """
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Lesd sd"""
+    """ Le df  """
     delays: List[float] = []
     all_delays: List[float] = []
-    for ff in range(n):
+    for i in range(n):
         delays.append(wait_random(max_delay))
-    for yy in asyncio.as_completed(delays):
-        er = await yy
-        all_delays.append(er)
-    return all_delay
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
